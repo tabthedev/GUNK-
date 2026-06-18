9b55d295d827f1feeb1e70f7f1d13a90f581fc2b
탭・TAB・タブ <me@tabthedev.kr>
2026-06-18 16:50:54 +0900

    screen속성 추가

--- src/classes/customWindow.py
+++ src/classes/customWindow.py
@@ -4,11 +4,12 @@ display.init()
 
 class CustomWindow:
     def __init__(self, windowName="GUNK!", initSize=(1280,720), initPos=(0,0), initFullscreen=False, initBorderless=True, initResizable=False, alwaysOnTop=False):
+        self._screen = display.set_mode(initSize)
         self._window = window.Window(windowName, initSize, initPos, fullscreen=initFullscreen, opengl=True, borderless=initBorderless, resizable=initResizable, allow_high_dpi=True, always_on_top=alwaysOnTop)
         pass
     
     def __getattr__(self, name):
-        return self._window[name]
+        return self._screen[name] or self._window[name]
     
     def setPosition(self, pos:tuple[int, int]):
         display.set_window_position(pos)
