a3b35c68a7ce5db207fa4e6750d293306bc22082
탭・TAB・タブ <me@tabthedev.kr>
2026-06-18 16:38:03 +0900

    기본 기능 추가

--- src/classes/customWindow.py
+++ src/classes/customWindow.py
@@ -1,1 +1,18 @@
+from pygame import display, window
 
+display.init()
+
+class CustomWindow:
+    def __init__(self, windowName="GUNK!", initSize=(1280,720), initPos=(0,0), initFullscreen=False, initBorderless=True, initResizable=False, alwaysOnTop=False):
+        self._window = window.Window(windowName, initSize, initPos, fullscreen=initFullscreen, opengl=True, borderless=initBorderless, resizable=initResizable, allow_high_dpi=True, always_on_top=alwaysOnTop)
+        pass
+    
+    def __getattr__(self, name):
+        return self._window[name]
+    
+    def setPosition(self, pos:tuple[int, int]):
+        display.set_window_position(pos)
+
+    def update(self):
+        self._window.flip()
+        
