0b99028c64ea9da6be378cd91c7f9a8a5e936da7
탭・TAB・タブ <me@tabthedev.kr>
2026-06-18 16:51:07 +0900

    동시 다중 윈도우 테스트

--- src/__init__.py
+++ src/__init__.py
@@ -11,15 +11,18 @@ window = customWindow.CustomWindow()
 dt = 0
 
 while Running:
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         # Running = False
-    #         break
-    #     elif event.type == pygame.WINDOWLEAVE:
-    #         print("나가지 마세요")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # Running = False
+            break
+        elif event.type == pygame.WINDOWLEAVE:
+            print("나가지 마세요")
+
+    window._screen.fill("purple")
 
     window.setPosition((512, 512))
     window.update()
+    window2.update()
 
     dt = clock.tick(60)/1000
 
