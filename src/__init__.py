5b03ab7c8dd7c8cd20022b2ed4a9a802654a0bc3
탭・TAB・タブ <me@tabthedev.kr>
2026-06-30 15:00:41 +0900

    add fps label

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,20 @@
 import pyglet
-from threading import Thread
-from src.atomic.Atom import Atom
-from src.classes import batch, clock, windows
+from src.atomic import animateCalculation
+from src.classes import batch, windows
 
 mainWindow = windows.mainWindow
 
 @mainWindow.event
 def on_draw():
     mainWindow.clear()
+
+    fpsLabel = pyglet.text.Label(f'FPS: {animateCalculation.fps}',
+        font_size=32,
+        x=16,y=16,
+        anchor_x='left', anchor_y='top'
+                                 )
+    fpsLabel.text
+
     batch.batch.draw()
         
 windows.InitWindowLocationAtom(mainWindow, True, (500,32))
