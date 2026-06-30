95fbb918f5f1f61f91072607614a165f464851be
탭・TAB・タブ <me@tabthedev.kr>
2026-06-30 11:13:33 +0900

    modify tick based wait function to ...뭐라하더라
    상대적? 절대적?

--- src/atomic/animateCalculation.py
+++ src/atomic/animateCalculation.py
@@ -4,12 +4,18 @@ from src.classes import clock
 deltaTime = 0
 tickrate = 60
 
+tickSpent = 0
+
 def WaitTicks(tick=1):
-    clock.clock.sleep(1/tickrate*tick)
+    # clock.clock.sleep(1/tickrate*tick)
+    curTick = tickSpent
+    while curTick+tick > tickSpent:
+        continue
 
 def TickCalculation():
-    global deltaTime
+    global deltaTime, tickSpent
     deltaTime = clock.clock.update_time()
+    tickSpent += 1
     WaitTicks()
 
 animThread = Thread(target=TickCalculation)
