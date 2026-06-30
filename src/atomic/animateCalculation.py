9b052093480e296817385fcfeb188701ae9c79f2
탭・TAB・タブ <me@tabthedev.kr>
2026-06-30 11:10:43 +0900

    create tick based sleep function

--- src/atomic/animateCalculation.py
+++ src/atomic/animateCalculation.py
@@ -4,11 +4,13 @@ from src.classes import clock
 deltaTime = 0
 tickrate = 60
 
+def WaitTicks(tick=1):
+    clock.clock.sleep(1/tickrate*tick)
+
 def TickCalculation():
     global deltaTime
     deltaTime = clock.clock.update_time()
-    clock.clock.sleep(1/tickrate)
-
+    WaitTicks()
 
 animThread = Thread(target=TickCalculation)
 animThread.daemon = True
